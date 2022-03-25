f255140577aa251837901411813d4d203f1b7267
nandotiamat <46244914+nandotiamat@users.noreply.github.com>
2022-03-25 16:23:36 +0100

    fix package name

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:schermatachat/widgets/personWidget.dart';
+import 'package:retireinvanvitelli/widgets/personWidget.dart';
 
 void main() {
   runApp(const MyApp());
